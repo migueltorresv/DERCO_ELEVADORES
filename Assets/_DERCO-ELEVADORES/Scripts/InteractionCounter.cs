7af42779be20b23e231a557f230d8f2e352bc68e
Miguel Torres <70416701@continental.edu.pe>
2023-08-04 15:34:15 -0500

    Comienzo parte evaluación
    Comienzo evaluación

---
+++ Assets/_DERCO-ELEVADORES/Scripts/InteractionCounter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionCounter : MonoBehaviour
+{
+    [SerializeField] private int _maxCount;
+    [SerializeField] private UnityEvent OnCountReached;
+
+    private int _count = 0;
+    private bool _isCountReached = false;
+
+    public void Add()
+    {
+        if (_isCountReached)
+            return;
+        
+        _count++;
+        if (_count >= _maxCount)
+        {
+            OnCountReached?.Invoke();
+            _isCountReached = true;
+        }
+    }
+}
